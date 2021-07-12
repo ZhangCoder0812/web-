64fcdfd997701dd1f66ac14b543c195a01f3a01b
MDZZ-LBJ <13041895753@163.com>
2021-07-12 22:45:32 +0800

    -

--- 1 - JS/7 - 面向对象/练习题1.js
+++ 1 - JS/7 - 面向对象/练习题1.js
@@ -1,22 +1,21 @@
-
 function C1(name) {
-  if (name) {
-    // 实例化的时候没有传参数 name属性不会被加在实例的私有属性上 访问name会找原型链上的name
-    this.name = name;
-  }
+    if (name) {
+        // 实例化的时候没有传参数 name属性不会被加在实例的私有属性上 访问name会找原型链上的name
+        this.name = name;
+    }
 }
 
 function C2(name) {
-  this.name = name;
-  /* 
+    this.name = name;
+    /* 
       实例化的时候没有传参数 但是name属性已经被加在实例私有属性上 
       值为undefined 访问name不会找原型上的name
   */
 }
 
 function C3(name) {
-  this.name = name || "join";
-  /* 
+    this.name = name || "join";
+    /* 
       实例化的时候没有传参数 但是name属性已经被加在实例私有属性上 
       不传的话使用 join 
   */
